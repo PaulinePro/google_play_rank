2a234f13d53560d94a741bb7844b0e47cec74e56
David <davidyen1124@gmail.com>
2014-04-01 17:05:28 +0800

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'google_play_rank',
+    packages = ['google_play_rank'],
+    version = '0.1',
+    description = 'Input an android package name, then it will help you find the ranking in the category.',
+    author = 'davidyen1124',
+    author_email = 'davidyen1124@gmail.com',
+    url = '',
+    download_url = '',
+    keywords = ['google play', 'ranking', 'android', 'package name'],
+)
